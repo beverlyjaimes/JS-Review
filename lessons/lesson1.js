84b7cd1086ef48b8badcca3c791679ac7f4544a2
Beverly Jaimes-Puente <52904488+beverlyjaimes@users.noreply.github.com>
2026-01-05 08:15:51 -0600

    initial commit

---
+++ lessons/lesson1.js
@@ -0,0 +1,26 @@
+//1.Hello Again
+console.log("Hello Again!")
+
+//Variables
+var firstName = "John"
+let lastName = "Smith"
+console.log(lastName)
+
+var age, dateOfBirth, sex
+age = "5"
+sex = "Male"
+console.log(age)
+
+//Constants
+const occupation = "Engineer"
+// occupation = "driver" //creates error, constant cannot be changed & cannot be created without value assigend 
+console.log(occupation)
+
+//Data Types
+var middleName ="David" //string
+var ageOfBrother = 25 //number
+var isHeMarried = false //boolean
+var yearsInMarriage = null //null 
+var numberOfCars = undefined //undefined
+
+adding stuff 
